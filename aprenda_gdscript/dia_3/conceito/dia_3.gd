00e04a648762e781e1908e474b5098e592103662
davi bandeira <fcodavibandeira@edu.unifor.br>
2023-02-28 10:54:29 -0300

    dia_1

---
+++ aprenda_gdscript/dia_3/conceito/dia_3.gd
@@ -0,0 +1,142 @@
+extends Node
+
+func _ready() -> void:
+	#Irei separar cada aula em funções diferentes para facilitar a visualização
+	#Você pode acessar a função pressionando o nome dela com o clique esquerdo do mouse
+	#enquanto segura o botão Control (Control + Clique esquerdo do mouse)
+	
+	aula_1()
+	#aula_2()
+	#aula_3()
+	#aula_4()
+	#aula_5()
+	#aula_6()
+	#aula_7()
+	
+	
+# Conteúdo auxiliar aula 1
+
+
+
+# Conteúdo auxiliar aula 1
+
+#Função da aula 1
+func aula_1() -> void:
+	print("Aula 1")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 2
+
+
+
+# Conteúdo auxiliar aula 2
+
+#Função da aula 2
+func aula_2() -> void:
+	print("Aula 2")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 3
+
+
+
+# Conteúdo auxiliar aula 3
+
+#Função da aula 3
+func aula_3() -> void:
+	print("Aula 3")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 4
+
+
+
+# Conteúdo auxiliar aula 4
+
+#Função da aula 4
+func aula_4() -> void:
+	print("Aula 4")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 5
+
+
+
+# Conteúdo auxiliar aula 5
+
+#Função da aula 5
+func aula_5() -> void:
+	print("Aula 5")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 6
+
+
+
+# Conteúdo auxiliar aula 6
+
+#Função da aula 6
+func aula_6() -> void:
+	print("Aula 6")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 7
+
+
+
+# Conteúdo auxiliar aula 7
+
+#Função da aula 7
+func aula_7() -> void:
+	print("Aula 7")
+	print("\n")
+	
+	
+	
+	print("\n")
+	
+	
+# Conteúdo auxiliar aula 8
+
+
+
+# Conteúdo auxiliar aula 8
+
+#Função da aula 8
+func aula_8() -> void:
+	print("Aula 8")
+	print("\n")
+	
+	
+	
+	print("\n")
